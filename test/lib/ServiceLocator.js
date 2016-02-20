8da3e556136fbf7b608666a039296abaae030054
Denis Rechkunov <mail@pragmader.me>
2016-02-20 12:27:26 +0100

    Drop constructor parameter injections.

--- test/lib/ServiceLocator.js
+++ test/lib/ServiceLocator.js
@@ -30,270 +30,151 @@
 
 'use strict';
 
-var assert = require('assert'),
-	ServiceLocator = require('../../lib/ServiceLocator');
+const assert = require('assert');
+const ServiceLocator = require('../../lib/ServiceLocator');
 
-// jscs:disable disallowUnusedParams
-function Constructor(
-		$testModule1, testArgument1, $testModule2, testArgument2, $testModule3
-	) {
-	this.args = arguments;
-}
-
-function TestModule1($testModule2, testArgument1) {
-	this.args = arguments;
-}
-
-function TestModule2($testModule3, testArgument1, $testModule4) {
-	this.args = arguments;
-}
-
-function TestModule3(testArgument1) {
-	this.args = arguments;
-}
-
-function TestModule4() {
-	this.args = arguments;
-}
-
-describe('ServiceLocator', function () {
-	describe('#register', function () {
-		it('should throw error if specified type is not a string', function () {
-			var locator = new ServiceLocator();
-			assert.throws(function () {
-				locator.register(null, function () {});
-			}, Error);
-		});
-
-		it('should throw error if specified constructor is not a function',
-			function () {
-				var locator = new ServiceLocator();
-
-				assert.throws(function () {
-					locator.register('typeName', null);
-				}, Error);
-			});
+/* eslint prefer-arrow-callback:0 */
+/* eslint max-nested-callbacks:0 */
+/* eslint require-jsdoc:0 */
+describe('ServiceLocator', function() {
+	var locator;
+	beforeEach(function() {
+		locator = new ServiceLocator();
 	});
 
-	describe('#unregister', function () {
-		it('should remove all registration of specified type', function () {
-			var locator = new ServiceLocator();
-			locator.register('type', function () {});
+	describe('#register', function() {
+		it('should throw an error if the specified type name is not a string', function() {
+			assert.throws(() => {
+				locator.register(null, function() {});
+			});
+		});
+
+		it('should throw an error if the specified implementation is not a function', function() {
+			assert.throws(() => {
+				locator.register('typeName', null);
+			});
+		});
+	});
+
+	describe('#unregister', function() {
+		it('should remove all registration of the specified type name', function() {
+			locator.register('type', function() {});
 			locator.registerInstance('type', {});
 			locator.unregister('type');
 
-			assert.throws(function () {
+			assert.throws(() => {
 				locator.resolve('type');
 			});
 		});
 	});
 
-	describe('#registerInstance', function () {
-		it('should register single instance for type', function () {
-			var locator = new ServiceLocator(),
-				instance = {},
-				instance2 = {};
-			locator.registerInstance('type', instance);
+	describe('#registerInstance', function() {
+		it('should register a single instance for the type name', function() {
+			const instance1 = {};
+			const instance2 = {};
+
+			locator.registerInstance('type', instance1);
 			locator.registerInstance('type', instance2);
-			var resolved = locator.resolve('type'),
-				resolved2 = locator.resolve('type');
-			assert.strictEqual(resolved, instance2);
+
+			const resolved1 = locator.resolve('type');
+			const resolved2 = locator.resolve('type');
+
+			assert.strictEqual(resolved1, instance2);
 			assert.strictEqual(resolved2, instance2);
 
-			var resolvedAll = locator.resolveAll('type');
-			assert.strictEqual(resolvedAll.length, 2);
-			assert.strictEqual(resolvedAll[0], instance2);
-			assert.strictEqual(resolvedAll[1], instance);
+			const resolvedList = locator.resolveAll('type');
+
+			assert.strictEqual(resolvedList.length, 2);
+			assert.strictEqual(resolvedList[0], instance2);
+			assert.strictEqual(resolvedList[1], instance1);
 		});
 	});
 
-	describe('#resolve', function () {
-
-		it('should resolve all registered modules', function () {
-			var locator = new ServiceLocator(),
-				rootParameters = {
-					testArgument1: 'Constructor.testArgument1',
-					testArgument2: 'Constructor.testArgument2'
-				},
-				module1Parameters = {
-					testArgument1: 'TestModule1.testArgument1'
-				},
-				module2Parameters = {
-					testArgument1: 'TestModule2.testArgument1'
-				},
-				module3Parameters = {
-					testArgument1: 'TestModule3.testArgument1'
-				};
-			locator.register('root', Constructor, rootParameters);
-			locator.register('testModule1', TestModule1, module1Parameters);
-			locator.register('testModule2', TestModule2, module2Parameters);
-			locator.register('testModule3', TestModule3, module3Parameters);
-			locator.register('testModule4', TestModule4);
-
-			var rootModule = locator.resolve('root');
-			assert.equal(rootModule instanceof Constructor, true,
-				'Wrong type resolution');
-			assert.equal(rootModule.args[0] instanceof TestModule1, true,
-				'Wrong type resolution');
-			assert.equal(rootModule.args[1], rootParameters.testArgument1,
-				'Wrong parameter resolution');
-			assert.equal(rootModule.args[2] instanceof TestModule2, true,
-				'Wrong type resolution');
-			assert.equal(rootModule.args[3], rootParameters.testArgument2,
-				'Wrong parameter resolution');
-			assert.equal(rootModule.args[4] instanceof TestModule3, true,
-				'Wrong type resolution');
-
-			// TestModule1
-			assert.equal(rootModule.args[0].args[0] instanceof TestModule2,
-				true,
-				'Wrong type resolution');
-			assert.equal(rootModule.args[0].args[1],
-				module1Parameters.testArgument1,
-				'Wrong parameter resolution');
-
-			// TestModule2
-			assert.equal(rootModule.args[2].args[0] instanceof TestModule3,
-				true,
-				'Wrong type resolution');
-			assert.equal(rootModule.args[2].args[1],
-				module2Parameters.testArgument1,
-				'Wrong parameter resolution');
-			assert.equal(rootModule.args[2].args[2] instanceof TestModule4,
-				true,
-				'Wrong type resolution');
-
-			// TestModule3
-			assert.equal(rootModule.args[4].args[0],
-				module3Parameters.testArgument1,
-				'Wrong parameter resolution');
-
-			// TestModule4
-			assert.equal(rootModule.args[2].args[2].args.length, 0,
-				'Wrong argument count');
-		});
-
-		it('should throw error if specified type was not found', function () {
-			var locator = new ServiceLocator();
-			assert.throws(function () {
-				locator.resolve('not exists');
-			}, Error);
-		});
-
-		it('should throw error if specified type is not a string', function () {
-			var locator = new ServiceLocator();
-			assert.throws(function () {
-				locator.resolve(null);
-			}, Error);
-		});
-
-		it('should return different instances each time',
-			function () {
-				var locator = new ServiceLocator();
-				locator.register('type', function () {}, {});
-
-				var instance1 = locator.resolve('type'),
-					instance2 = locator.resolve('type');
-
-				assert.notEqual(instance1, instance2);
-			});
-
-		it('should return the same instance each time if it is as singleton',
-			function () {
-				var locator = new ServiceLocator();
-				locator.register('type', function () {}, {}, true);
-
-				var instance1 = locator.resolve('type'),
-					instance2 = locator.resolve('type');
-
-				assert.equal(instance1, instance2);
-			});
-
-		it('should resolve unspecified parameters as undefined', function () {
-			var locator = new ServiceLocator();
-			locator.register('type', function (test) {
-				assert.strictEqual(test, undefined);
-			});
-
-			locator.resolve('type');
-		});
-	});
-
-	describe('#resolveAll', function () {
-		it('should resolve all registered implementations of type',
-			function () {
-				var locator = new ServiceLocator();
-
-				function Implementation1() { }
-
-				function Implementation2() { }
-
-				function Implementation3() { }
-
-				function AnotherImplementation() { }
-
-				locator.register('type', Implementation1);
-				locator.register('type', Implementation2);
-				locator.register('type', Implementation3);
-				locator.register('anotherType', AnotherImplementation);
-
-				var instances = locator.resolveAll('type');
-				assert.equal(instances.length, 3, 'Wrong instance count');
-				assert.equal(instances[0] instanceof Implementation3, true,
-					'Wrong type resolution');
-				assert.equal(instances[1] instanceof Implementation2, true,
-					'Wrong type resolution');
-				assert.equal(instances[2] instanceof Implementation1, true,
-					'Wrong type resolution');
-			});
-
-		it('should return empty array if specified type was not found',
-			function () {
-				var locator = new ServiceLocator(),
-					resolved = locator.resolveAll('not exists');
-
-				assert.strictEqual(resolved instanceof Array, true);
-				assert.strictEqual(resolved.length, 0);
-			});
-
-		it('should throw error if specified type is not a string', function () {
-			var locator = new ServiceLocator();
-			assert.throws(function () {
-				locator.resolveAll(null);
-			}, Error);
-		});
-	});
-
-	describe('#resolveInstance', function () {
-		it('should resolve instance of specified constructor and parameters',
-			function () {
-				var locator = new ServiceLocator(),
-					testArgument = 'testArgumentValue';
-
-				function TestModule() { }
-
-				function ModuleToResolve($testType, testArgument) {
-					assert.equal($testType instanceof TestModule, true);
-					assert.strictEqual(testArgument, testArgument);
+	describe('#resolve', function() {
+		it('should properly resolve an instance of the registered class implementation', function() {
+			class Implementation {
+				constructor(locator) {
+					this.locator = locator;
 				}
+			}
 
-				locator.register('testType', TestModule);
-				var instance = locator.resolveInstance(ModuleToResolve, {
-					testArgument: testArgument
-				});
+			locator.register('test', Implementation);
+			const instance = locator.resolve('test');
+			assert.strictEqual(instance instanceof Implementation, true);
+			assert.strictEqual(instance.locator, locator);
+		});
 
-				assert.equal(instance instanceof ModuleToResolve, true);
+		it('should properly resolve an instance of the registered constructor/prototype implementation', function() {
+			function Implementation(locator) {
+				this.locator = locator;
+			}
+
+			locator.register('test', Implementation);
+			const instance = locator.resolve('test');
+			assert.strictEqual(instance instanceof Implementation, true);
+			assert.strictEqual(instance.locator, locator);
+		});
+
+		it('should throw an error if the specified type name was not found', function() {
+			assert.throws(function() {
+				locator.resolve('not exists');
 			});
+		});
 
-		it('should resolve instance of empty constructor and parameters',
-			function () {
-				var locator = new ServiceLocator();
-
-				function ModuleToResolve() { }
-
-				var instance = locator.resolveInstance(ModuleToResolve);
-
-				assert.equal(instance instanceof ModuleToResolve, true);
+		it('should throw an error if the specified type name is not a string', function() {
+			assert.throws(function() {
+				locator.resolve(null);
 			});
+		});
+
+		it('should return different instances every time', function() {
+			locator.register('type', function() {});
+
+			const instance1 = locator.resolve('type');
+			const instance2 = locator.resolve('type');
+
+			assert.notEqual(instance1, instance2);
+		});
+
+		it('should return the same instance every time if it is a singleton', function() {
+			locator.register('type', function() {}, true);
+
+			const instance1 = locator.resolve('type');
+			const instance2 = locator.resolve('type');
+
+			assert.equal(instance1, instance2);
+		});
+	});
+
+	describe('#resolveAll', function() {
+		it('should resolve all registered implementations of the type name', function() {
+			class Implementation1 { }
+			function Implementation2() { }
+			class Implementation3 { }
+			function AnotherImplementation() { }
+
+			locator.register('type', Implementation1);
+			locator.register('type', Implementation2);
+			locator.register('type', Implementation3);
+			locator.register('anotherType', AnotherImplementation);
+
+			var instances = locator.resolveAll('type');
+			assert.strictEqual(instances.length, 3);
+			assert.strictEqual(instances[0] instanceof Implementation3, true);
+			assert.strictEqual(instances[1] instanceof Implementation2, true);
+			assert.strictEqual(instances[2] instanceof Implementation1, true);
+		});
+
+		it('should throw an error if the specified type was not found', function() {
+			assert.throws(() => {
+				locator.resolveAll('not exists');
+			});
+		});
+
+		it('should throw an error if the specified type is not a string', function() {
+			assert.throws(() => {
+				locator.resolveAll(null);
+			});
+		});
 	});
 });
